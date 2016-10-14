0031d9e1886ab74cebd17920463d1fbb81ad0cc3
Alessandro Costa <alessandro.asac@gmail.com>
2016-10-14 14:04:07 -0300

    Finalizada Parte 2

--- jogo.js
+++ jogo.js
@@ -1,8 +1,57 @@
+'use strict';
+
+const MAX_PULOS = 3;
+
 // Variáveis do jogo
-var canvas, ctx, ALTURA, LARGURA, frames = 0;
+let canvas, ctx, ALTURA, LARGURA, frames = 0;
+
+let chao = {
+  y: 550,
+  altura: 50,
+  cor: '#ffdf70',
+
+  desenhar: function() {
+    ctx.fillStyle = this.cor;
+    ctx.fillRect(0, this.y, LARGURA, this.altura);
+  }
+};
+
+let bloco = {
+  x: 50,
+  y: 0,
+  altura: 50,
+  largura: 50,
+  cor: '#ff4e4e',
+  gravidade: 1.5,
+  velocidade: 0,
+  forcaDoPulo: 15,
+  qntPulos: 0,
+
+  atualizar: function() {
+    this.velocidade += this.gravidade;
+    this.y += this.velocidade;
+
+    if (this.y > chao.y - this.altura) {
+      this.y = chao.y - this.altura;
+      this.qntPulos = 0;
+    }
+  },
+
+  pular: function() {
+    if (this.qntPulos < MAX_PULOS) {
+      this.velocidade = -this.forcaDoPulo;
+      this.qntPulos++;
+    }
+  },
+
+  desenhar: function() {
+    ctx.fillStyle = this.cor;
+    ctx.fillRect(this.x, this.y, this.largura, this.altura);
+  }
+};
 
 function clique(e) {
-  alert('Clicou!');
+  bloco.pular();
 }
 
 function main() {
@@ -35,11 +84,16 @@ function rodar() {
 
 function atualizar() {
   frames++;
+
+  bloco.atualizar();
 }
 
 function desenhar() {
   ctx.fillStyle = '#50beff';
   ctx.fillRect(0, 0, LARGURA, ALTURA);
+
+  chao.desenhar();
+  bloco.desenhar();
 }
 
 // Inicializa o jogo
