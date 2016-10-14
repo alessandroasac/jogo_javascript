89b9f7f4e8e884617fbef4a4b34dabddc6be9881
Alessandro Costa <alessandro.asac@gmail.com>
2016-10-14 17:23:07 -0300

    Finalizada parte 3. Feitas modificações para ES6

--- jogo.js
+++ jogo.js
@@ -1,100 +1,136 @@
 'use strict';
 
 const MAX_PULOS = 3;
+const ALTURA = window.innerWidth > 600 ? 600 : window.innerHeight;
+const LARGURA = window.innerWidth > 600 ? 600 : window.innerWidth;
+const GRAVIDADE = 1.5;
 
-// Variáveis do jogo
-let canvas, ctx, ALTURA, LARGURA, frames = 0;
+class Chao {
 
-let chao = {
-  y: 550,
-  altura: 50,
-  cor: '#ffdf70',
+  constructor() {
+    this.y = 550;
+    this.altura = 50;
+    this.cor = '#ffdf70';
+  }
 
-  desenhar: function() {
+  desenhar(ctx) {
     ctx.fillStyle = this.cor;
     ctx.fillRect(0, this.y, LARGURA, this.altura);
   }
-};
+}
 
-let bloco = {
-  x: 50,
-  y: 0,
-  altura: 50,
-  largura: 50,
-  cor: '#ff4e4e',
-  gravidade: 1.5,
-  velocidade: 0,
-  forcaDoPulo: 15,
-  qntPulos: 0,
+class Bloco {
 
-  atualizar: function() {
-    this.velocidade += this.gravidade;
+  constructor() {
+    this.x           = 50,
+    this.y           = 0,
+    this.altura      = 50,
+    this.largura     = 50,
+    this.cor         = '#ff9239',
+    this.velocidade  = 0,
+    this.forcaDoPulo = 23.6,
+    this.qntPulos    = 0;
+
+  }
+
+  atualizar(chao) {
+    this.velocidade += GRAVIDADE;
     this.y += this.velocidade;
 
     if (this.y > chao.y - this.altura) {
       this.y = chao.y - this.altura;
       this.qntPulos = 0;
     }
-  },
+  }
 
-  pular: function() {
+  pular() {
     if (this.qntPulos < MAX_PULOS) {
       this.velocidade = -this.forcaDoPulo;
       this.qntPulos++;
     }
-  },
+  }
 
-  desenhar: function() {
+  desenhar(ctx) {
     ctx.fillStyle = this.cor;
     ctx.fillRect(this.x, this.y, this.largura, this.altura);
   }
-};
-
-function clique(e) {
-  bloco.pular();
 }
 
-function main() {
-  ALTURA = window.innerHeight;
-  LARGURA = window.innerWidth;
+class Obstaculo {
 
-  if (LARGURA > 600) {
-    LARGURA = 600;
-    ALTURA = 600;
+  constructor() {
+    this._obs = [];
+    this.cores = ['#ffbc1c', '#ff1c1c', '#ff85e1', '#52a7ff', '#78ff5d'];
   }
 
-  canvas = document.createElement('canvas');
-  canvas.width = LARGURA;
-  canvas.height = ALTURA;
-  canvas.style.border = '1px solid #000';
+  inserir() {
+    this._obs.push({
+      x: LARGURA,
+      largura: 30 + Math.floor(21 * Math.random()),
+      altura: 30 + Math.floor(120 * Math.random()),
+      cor: this.cores[Math.floor(5 * Math.random())],
 
-  ctx = canvas.getContext('2d');
-  document.body.appendChild(canvas);
-  document.addEventListener('mousedown', clique);
+      atualizar: function() {
 
-  rodar();
+      },
+
+      desenhar: function() {
+        for (let i = 0; i < this._obs.length; i++) {
+          let obs = this._obs[i];
+          ctx.fillStyle = obs.cor;
+          ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura);
+        }
+      }
+    });
+  }
 }
 
-function rodar() {
-  atualizar();
-  desenhar();
+class Jogo {
 
-  window.requestAnimationFrame(rodar);
+  constructor() {
+    this.chao = new Chao();
+    this.bloco = new Bloco();
+    this.canvas = this.criarCanvas();
+    this.ctx = this.canvas.getContext('2d');
+    this.frames = 0;
+  }
+
+  inicializar() {
+    document.body.appendChild(this.canvas);
+    document.addEventListener('mousedown', () => this.bloco.pular());
+    this._rodar();
+  }
+
+  _rodar() {
+    this._atualizar(this.bloco);
+    this._desenhar(this.bloco, this.ctx, this.chao);
+
+    window.requestAnimationFrame(() => this._rodar(this.bloco, this.ctx, this.chao));
+  }
+
+  _atualizar() {
+    this.frames++;
+
+    this.bloco.atualizar(this.chao);
+  }
+
+  _desenhar() {
+    this.ctx.fillStyle = '#80daff';
+    this.ctx.fillRect(0, 0, LARGURA, ALTURA);
+
+    this.chao.desenhar(this.ctx);
+    this.bloco.desenhar(this.ctx);
+  }
+
+  criarCanvas() {
+    const canvas = document.createElement('canvas');
+    canvas.width = LARGURA;
+    canvas.height = ALTURA;
+    canvas.style.border = '1px solid #000';
+
+    return canvas;
+  }
+
 }
 
-function atualizar() {
-  frames++;
-
-  bloco.atualizar();
-}
-
-function desenhar() {
-  ctx.fillStyle = '#50beff';
-  ctx.fillRect(0, 0, LARGURA, ALTURA);
-
-  chao.desenhar();
-  bloco.desenhar();
-}
-
-// Inicializa o jogo
-main();
+new Jogo().inicializar();
